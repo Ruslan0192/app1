54e39ae8138bc7a0cc12d8c7b3be28a5e3c3d093
Ruslan0192 <ruslan.salmin@gmail.com>
2024-12-20 08:31:27 +0300

    debug_toolbar

--- hubstudio/urls.py
+++ hubstudio/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.core.checks import DEBUG
 from django.urls import include, path
+
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 
 urlpatterns = [
@@ -24,3 +27,5 @@ urlpatterns = [
     path('catalog/', include('goods.urls', namespace='catalog'))
 
 ]
+if DEBUG:
+    urlpatterns += debug_toolbar_urls()
